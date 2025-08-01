5216915f91c06e610d60735506cb37ea2b1d7dd2
Naman Kumar <parnamnkh1@gmail.com>
2025-08-01 23:52:26 +0530

    Refactor project structure and enhance basic functionality.

---
+++ config/config.js
@@ -0,0 +1,22 @@
+const cors = require('cors');
+
+ const configuration= () =>{
+    return cors({
+        origin: (origin, callback) => {
+            const allowedOrigins = ['http://localhost:3000', 'http://localhost:5173'];
+            if (allowedOrigins.includes(origin) || !origin) {
+                callback(null, true);
+            } else {
+                callback(new Error('Not allowed by CORS'));
+            }
+        },
+        methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+        allowedHeaders: ['Content-Type', 'Authorization','Accept-Version'],
+        credentials: true,
+        maxAge: 86400,
+        preflightContinue: false,
+        optionsSuccessStatus: 204,
+        exposedHeaders: ['Content-Range', 'X-Total-Count'],
+  
+    })
+ }
